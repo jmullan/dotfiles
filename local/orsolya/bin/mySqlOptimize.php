a57d567f19f1510b3974b996ffd716fc55c43bd4
Jesse Mullan <jmullan@visi.com>
2014-03-03 12:01:01 -0800

    update mysql optimization script

--- local/orsolya/bin/mySqlOptimize.php
+++ local/orsolya/bin/mySqlOptimize.php
@@ -7,13 +7,16 @@ $databases = $dbm->getColumn("SHOW DATABASES WHERE `Database` NOT IN ('informati
 
 $queries = array();
 foreach ($databases as $database) {
-  $tables = $dbm->getColumn("SHOW TABLES FROM `$database`");
-  foreach ($tables as $table) {
-    $queries[] = "OPTIMIZE TABLE `$database`.`$table`";
-    $queries[] = "ANALYZE TABLE `$database`.`$table`";
-  }
+    if ($database == 'performance_schema') {
+        continue;
+    }
+    $tables = $dbm->getColumn("SHOW TABLES FROM `$database`");
+    foreach ($tables as $table) {
+        $queries[] = "OPTIMIZE TABLE `$database`.`$table`";
+        $queries[] = "ANALYZE TABLE `$database`.`$table`";
+    }
 }
 foreach ($queries as $query) {
-  #echo "$query\n";
-  $dbm->query($query);
+    #echo "$query\n";
+    $dbm->query($query);
 }
