5e6d132809dd5144bb6243293718e54964d9c025
Jesse Mullan <jmullan@visi.com>
2014-05-28 10:09:28 -0700

    js3-mode

--- Makefile
+++ Makefile
@@ -13,6 +13,7 @@ portable/.dircolors: submodules
 
 install: submodules portable/.dircolors portable/.emacs.d/themes/color-theme-solarized portable/bin/back
 	sh meta/bin/install.sh $(PWD)
+	emacs -batch -f batch-byte-compile submodules/js3-mode/*.el >/dev/null 2>&1
 	emacs -batch -f batch-byte-compile portable/lib/emacs/lisp/*.el >/dev/null 2>&1
 
 .PHONY: install submodules
--- portable/.xemacs/custom.el
+++ portable/.xemacs/custom.el
@@ -1,19 +1,23 @@
 (custom-set-variables
-  ;; custom-set-variables was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
  '(gnuserv-program (concat exec-directory "/gnuserv"))
  '(inhibit-startup-screen t)
  '(js2-bounce-indent-p t)
  '(js2-use-ast-for-indentation-p t)
+ '(js3-allow-keywords-as-property-names nil)
+ '(js3-indent-level 4)
+ '(js3-max-columns 149)
+ '(js3-strict-missing-semi-warning t)
  '(sql-indent-maybe-tab t)
  '(sql-indent-offset 4)
  '(sql-product (quote mysql)))
 (custom-set-faces
-  ;; custom-set-faces was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
  '(default ((t (:inherit nil :stipple nil :background "black" :foreground "white" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 116 :width normal :foundry "unknown" :family "DejaVu Sans Mono"))))
- '(mmm-default-submode-face ((t (:background "gray40")))))
+ '(mmm-default-submode-face ((t (:background "gray40"))) t))
