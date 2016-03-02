654a63be431f426e0bdc3b6e84e975be79c4df00
Jesse Mullan <jmullan@visi.com>
2016-03-01 16:51:57 -0800

    updates

--- portable/bin/fix_more_things.sh
+++ portable/bin/fix_more_things.sh
@@ -4,4 +4,5 @@ INDENT=`current_editor_config indent_size`
 fix_simple_whitespace.sh ${1+"$@"}
 unify --quote "'" --in-place ${1+"$@"}
 docformatter -i --wrap-summaries 120 --wrap-descriptions 110 --no-blank ${1+"$@"}
+autopep8 -i --ignore-local-config --global-config ../.autopep8 --indent-size="$INDENT" --select=E201,E202,E203,E211,E221,E222,E223,E224,E225,E226,E227,E228,E231,E241,E242,E251,E261,E271,E272,E273,E274 ${1+"$@"}
 autopep8 -i --ignore-local-config --global-config ../.autopep8 --indent-size="$INDENT" --select=E101 ${1+"$@"}
