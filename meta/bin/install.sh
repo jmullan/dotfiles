0745f32aa7f0747915511a418a3b8dd269516ec1
Jesse Mullan <jmullan@gmail.com>
2022-05-30 17:52:42 -0700

    tweak install file?

--- meta/bin/install.sh
+++ meta/bin/install.sh
@@ -8,26 +8,26 @@ if [ ! -e "$SRC_DIR" ] ; then
     echo "Your source directory argument must refer to a source directory"
     exit
 fi
-for FULLFILE in `find "$SRC_DIR" -mindepth 1 -maxdepth 1 -not -name '.git' | grep -v '~$' ` ; do
-    BASENAME=`basename "$FULLFILE"`
-    if [ ! -e ~/"$BASENAME" ] ; then
+for FULLFILE in $(find "$SRC_DIR" -mindepth 1 -maxdepth 1 -not -name '.git' | grep -v '~$') ; do
+    BASENAME=$(basename "$FULLFILE")
+    if [ ! -e ~/"${BASENAME}" ] ; then
 	echo "Linking $FULLFILE to ~/$BASENAME";
 	ln -s "$FULLFILE" ~/"$BASENAME";
     else
 	ls -lAd ~/"$BASENAME";
     fi
 done
-SRC_DIR=$1"/local/"`hostname`
+SRC_DIR=$1"/local/"$(hostname)
 if [ -e "$SRC_DIR" ] ; then
-    for FULLFILE in `find "$SRC_DIR" -mindepth 1 -maxdepth 1 -not -name '.git' | grep -v '~$' | grep -v '.bak'` ; do
-        BASENAME=`basename "$FULLFILE"`
-        LOCALFILE=`hostname`"_$BASENAME"
+    for FULLFILE in $(find "$SRC_DIR" -mindepth 1 -maxdepth 1 -not -name '.git' | grep -v '~$' | grep -v '.bak') ; do
+        BASENAME=$(basename "${FULLFILE}")
+        LOCALFILE="$(hostname)_$BASENAME)"
         echo ~/"$LOCALFILE"
         if [ ! -e ~/"$LOCALFILE" ] ; then
 	    echo "Linking $LOCALFILE to ~/$LOCALFILE";
 	    ln -s "$FULLFILE" ~/"$LOCALFILE";
         else
-	    ls -lAd ~/"$LOCALFILE";
+	    ls -lAd ~/"${LOCALFILE}";
         fi
     done
 fi
@@ -37,6 +37,6 @@ fi
 if [ -e ~/Library ]; then
     mkdir -p ~/Library/KeyBindings
     if [ ! -e ~/Library/KeyBindings/DefaultKeyBinding.dict ] ; then
-        ln -s `pwd`/local/osx/Library/KeyBindings/DefaultKeyBinding.dict ~/Library/KeyBindings/DefaultKeyBinding.dict
+        ln -s "$(pwd)/local/osx/Library/KeyBindings/DefaultKeyBinding.dict" "~/Library/KeyBindings/DefaultKeyBinding.dict"
     fi
 fi
